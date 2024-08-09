d9bd849dc2c0fd2d896ba9fd3501f24e3dd3bfd0
chaudharianiruddha <chaudharianiruddha19@gmail.com>
2024-08-09 19:56:31 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -206,21 +206,4 @@ for (let i = 0; i < navigationLinks.length; i++) {
     }
 
   });
-
-
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
-
-// Close modal when clicking outside the image
-modal.addEventListener("click", function(event) {
-  if (event.target === modal) {
-    modal.style.display = "none";
-  }
-});
 }
