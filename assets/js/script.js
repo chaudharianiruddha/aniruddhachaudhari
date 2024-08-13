7fb397051fdb9038f6a61ead07c153fb57490b40
chaudharianiruddha <chaudharianiruddha19@gmail.com>
2024-08-13 12:07:03 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -138,6 +138,13 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+//Share button
+document.getElementById('share-button').addEventListener('click', function () {
+  var shareOptions = document.getElementById('share-options');
+  shareOptions.style.display = shareOptions.style.display === 'block' ? 'none' : 'block';
+});
+
+
 // Page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
